87d6614e89ed57a149e13766d2049a0b82f0c12b
Manminder Singh <mmsr.aws@gmail.com>
2022-07-27 17:48:39 -0700

    for-loop

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+if n % 3 == 0:
+    print("weird")
+elif n in range(2,6) % 2 ==0:
+    print("Not Weird")
---
+++ forloop.py
@@ -0,0 +1,7 @@
+i = int(input("Enter the number below 5"))
+
+while i <= 5:
+    print(i)
+    if i > 5:
+       break
+    i += 1
