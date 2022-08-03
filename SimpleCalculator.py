998665a43beca4a7864a5f0535dd529f6a1a76cf
Manminder Singh <mmsr.aws@gmail.com>
2022-08-02 23:50:49 -0700

    simple code added

---
+++ SimpleCalculator.py
@@ -0,0 +1,21 @@
+import  math
+from re import M
+print("-" * 90)
+print("Please enter 2 digits below")
+print("-" * 90)
+x = float(input("Please first integer: "))
+y = float(input("Please second integer: "))
+
+a = x + y
+m = x * y
+s = x - y
+d = x / y
+mod = x // y
+
+print("*" * 90)
+print("Addition will be : ", a)
+print("multiplication will be: ", m)
+print("Subtraction will be: ", s)
+print("Division will be: ", d)
+print("Modulous will be: ", mod)
+print("*" * 90)
